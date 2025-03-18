c98a9fd62bbf6f35cfdab29b2e355dbc67e1d37b
zayyni <zainulislam.733@gmail.com>
2025-03-19 00:27:30 +0500

    refreshed some basic javascript operations

---
+++ JAVASCRIPT/02-JS-Operators/operators.js
@@ -0,0 +1,17 @@
+/**
+ * 1.arithmetic + - % / *
+ * 2.shorthand += -= *=
+ * 3. logical operators && || 
+ * 4. Conditional : < > <= >= !==
+ * 5. ternary ?:
+ * 6. incr, decr ++ --
+ * 7. equality == , ===
+*/
+
+
+let num1 = 100;
+let num2 = 20;
+console.log(`Sum : ${num1 + num2}`);
+console.log(`Sub : ${num1 - num2}`);
+console.log(`Mul : ${num1 * num2}`);
+console.log(`Div : ${num1 / num2}`);
