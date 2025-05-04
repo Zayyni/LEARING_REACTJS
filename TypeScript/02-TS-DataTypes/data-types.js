48c31222eca98d9021a2c6998514dca8e81973d6
zayyni <zainulislam.733@gmail.com>
2025-05-04 22:07:46 +0500

    implemented basics operations using typescript

---
+++ TypeScript/02-TS-DataTypes/data-types.js
@@ -0,0 +1,15 @@
+// string
+var employeeName = "John";
+console.log(employeeName);
+//number
+var employeeAge = 40;
+console.log(employeeAge);
+//boolean
+var isManager = true;
+console.log(isManager);
+var mobile = {
+    brand: "Nokia",
+    color: "Blue",
+    price: 23445
+};
+console.log(mobile);
---
+++ TypeScript/03-TS-Operators/operators.js
@@ -0,0 +1,14 @@
+var num1 = 10;
+var num2 = 20;
+var result = num1 + num2;
+console.log("result : ".concat(result));
+// conditional 
+var marks = 76;
+var theResult = "";
+if (marks <= 35) {
+    theResult = "You failed the Exam";
+}
+else {
+    theResult = "You passed the Exam";
+}
+console.log(theResult);
